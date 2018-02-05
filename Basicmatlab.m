fc6852d65990137fb9758b9d9ddb7c2af0640b74
Anish0494 <36161110+Anish0494@users.noreply.github.com>
2018-02-05 20:00:01 +0530

    Update Basicmatlab.m

--- Basicmatlab.m
+++ Basicmatlab.m
@@ -1,17 +1,23 @@
 %reading the image
 f=imread('download.jpg');
+
 %finding the size of the image
 size(f)
+
 % to store number of rows and column of image
 [M,N]= size(f);
+
 % displaying additional information about the image
 whos f;
+
 %dispalying 2 image side by side
 subplot(1,2,1);
+
 % showing image f with intensity g if not mention it will take 256
 low=160;
 high=230;
 imshow(f,[low high])
+
 % showing image 
 subplot(1,2,2);
 imshow(f)
